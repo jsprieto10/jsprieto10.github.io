9d268018275a02b4f5c50caa92de46b3f4d89dc3
jsprieto10 <js.prieto10@uniandes.edu.co>
2018-02-19 16:05:01 -0500

    revert forms

--- js/form.js
+++ js/form.js
@@ -11,25 +11,16 @@ $('#send').click(function(){
   var email = $('#email');
   var phone = $('#celular');
   var message = $('#mensaje');
-<<<<<<< HEAD
-  name = $('#nombre').val('');
-   email = $('#email').val('');
-   phone = $('#celular').val('');
-   message = $('#mensaje').val('');
-   alert('se ha enviado correctamente');
-=======
->>>>>>> parent of f16cd3c... Update form.js
   // envio del correo usando la librería
   emailjs.send("gmail","template_BZGiJMnO",{nombre: name.val(), correo: email.val(), celular: phone.val(), mensaje: message.val()})
   .then(function(response) {
     // se ejecuta de manera sincronica esta función que vacia los elementos del arreglo
-<<<<<<< HEAD
-=======
+    alert('se ha enviado correctamente');
     name = $('#nombre').val('');
     email = $('#email').val('');
     phone = $('#celular').val('');
     message = $('#mensaje').val('');
-    alert('se ha enviado correctamente');
+    
 >>>>>>> parent of f16cd3c... Update form.js
     console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
   },function(err) {console.log("FAILED. error=", err);});
