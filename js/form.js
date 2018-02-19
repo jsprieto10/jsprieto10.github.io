f16cd3c8cb28342e5eb3d18b8cc3cbaa859bd360
jsprieto10 <js.prieto10@uniandes.edu.co>
2018-02-19 10:30:25 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -11,14 +11,14 @@ $('#send').click(function(){
   var email = $('#email');
   var phone = $('#celular');
   var message = $('#mensaje');
+  name = $('#nombre').val('');
+   email = $('#email').val('');
+   phone = $('#celular').val('');
+   message = $('#mensaje').val('');
   // envio del correo usando la librería
   emailjs.send("gmail","template_BZGiJMnO",{nombre: name.val(), correo: email.val(), celular: phone.val(), mensaje: message.val()})
   .then(function(response) {
     // se ejecuta de manera sincronica esta función que vacia los elementos del arreglo
-    name = $('#nombre').val('');
-    email = $('#email').val('');
-    phone = $('#celular').val('');
-    message = $('#mensaje').val('');
     alert('se ha enviado correctamente');
     console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
   },function(err) {console.log("FAILED. error=", err);});
